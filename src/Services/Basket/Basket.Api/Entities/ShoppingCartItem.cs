f5ca781321e5f21c1c23afc10782f0d9943c61af
njeziofficial-1 <96592589+njeziofficial-1@users.noreply.github.com>
2025-07-13 13:17:57 +0100

    Add Docker support for Basket.Api service
    Introduced Dockerfile and related configuration for Basket.Api, enabling containerized development and deployment. Updated project file, launch settings, and docker-compose files to support Redis for basketdb and integrate Basket.Api into the multi-service environment.

--- src/Services/Basket/Basket.Api/Entities/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCartItem.cs
@@ -7,12 +7,4 @@ public class ShoppingCartItem
     public string Colour { get; set; }
     public double Price { get; set; }
     public int Quantity { get; set; }
-    //public ShoppingCartItem(string productId, string productName, string colour, double price, int quantity)
-    //{
-    //    ProductId = productId;
-    //    ProductName = productName;
-    //    Price = price;
-    //    Quantity = quantity;
-    //    Colour = colour;
-    //}
 }
